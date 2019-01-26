573b43dc483ef1a56cc2b4ae119cd7455c5195a4
Rai <rai.mckenziee@gmail.com>
2019-01-26 15:58:10 -0800

    added express dependency

--- nwHacksBackend/app.js
+++ nwHacksBackend/app.js
@@ -5,8 +5,13 @@
 
 var express = require("express");
 var app = express();
-var port = 80; //Port to run webserver on.
+var port = 8080; //Port to run webserver on.
 
-app.listen(80, () => {
-	console.log("Server running on port 80");
+app.listen(port, () => {
+	console.log("Server running on port "+port);
+});
+
+app.get("/api/signin", (req, res, next) => {
+	res.json(req.params);
+	//res.json(["Tony","Lisa","Michael","Ginger","Food"]);
 });

f49587be34d06f1649c6cd2cc5f85a0720c9c775
Rai <rai.mckenziee@gmail.com>
2019-01-27 00:52:42 -0800

    Added midpoint algorithm

--- nwHacksBackend/algorithms.js
+++ nwHacksBackend/algorithms.js
@@ -8,10 +8,10 @@ class Location {
 /**
  * Finds the shortest path between a cluster of start locations
  * and a cluster of end locations
- * 
- * @param {Array.<Location>} startLocs 
- * @param {Array.<Location>} endLocs 
- * 
+ *
+ * @param {Array.<Location>} startLocs
+ * @param {Array.<Location>} endLocs
+ *
  * @returns {Object{start<Location>, end<Location>}}
  */
 function shortest_path(startLocs, endLocs) {
@@ -33,7 +33,25 @@ function shortest_path(startLocs, endLocs) {
 	};
 }
 
+/**
+ * Finds the midpoint
+ *
+ * @param {Array.<Location>} locs
+ *
+ * @returns {midpoint<Location>,}
+ */
+function shortest_path(locs) {
+	const startLats = locs.map(loc => loc.lat);
+	const startLons = locs.map(loc => loc.lon);
+
+	const latAvg = lats.reduce((total, next) => total + next) / lats.length;
+	const lonAvg = lons.reduce((total, next) => total + next) / lons.length;
+
+	return new Location(latAvg, lonAvg);
+}
+
 module.exports = {
 	Location,
 	shortest_path,
+	midpoint,
 };

57a4f4d6c14d6de76fbcadcb9d01d12721ac2466
Wesley Chang <wesleycanada@gmail.com>
2019-01-26 23:48:25 -0800

    Refactored with settings

--- nwHacksBackend/compute.js
+++ nwHacksBackend/compute.js
@@ -5,18 +5,14 @@
 var frequency = 15; //in seconds
 
 const mysql = require("mysql");
+const settings = require("./settings");
 
-const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'admin',
-    password: 'admin',
-    database: 'rideshare',
-});
+const conn = mysql.createConnection(settings.CONN_INFO);
 
-console.log(`Running every `+frequency+` seconds...`);
+console.log("Running every "+frequency+" seconds...");
 
 //Recurring task at frequency.
 setInterval(function() {
-	console.log('Running...');
+	console.log("Running...");
 
 }, frequency * 1000);

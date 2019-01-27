70f780081e3a4d1b6d83e6cc08ee82b5d2c872ec
Rai <rai.mckenziee@gmail.com>
2019-01-26 16:21:03 -0800

    Added first endpoint

--- nwHacksBackend/app.js
+++ nwHacksBackend/app.js
@@ -11,8 +11,35 @@ app.listen(port, () => {
 	console.log("Server running on port "+port);
 });
 
+/**
+ * Initial signon request
+ * Requires:
+ * 		username(string)
+ */
 app.get("/api/signin", (req, res, next) => {
-	res.json(req.query);
+	var param = req.query;
+
+	if (!('username' in param)) {
+		res.json({
+			status: 'ERROR',
+			message: 'No username specified.',
+			payload: {},
+		});
+		return;
+	}
+
+	/*
+	TODO: Enter username into database, return userID.
+	 */
+
+	var userID = 1234;
+
+	 res.json({
+ 		status: 'OK',
+ 		message: 'All good',
+		payload: {
+			userID: userID,
+		},
+ 	});
 	return;
-	//res.json(["Tony","Lisa","Michael","Ginger","Food"]);
 });

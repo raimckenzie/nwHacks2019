8df17976c4f5beb66decc5d2aa81b28d32ec062b
Wesley Chang <wesleycanada@gmail.com>
2019-01-27 09:00:49 -0800

    initial commit

---
+++ nw_hacks_react/src/modules/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Main;

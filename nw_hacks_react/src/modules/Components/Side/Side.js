50fb8f6a00e030c2a42f4b1075a149de41773063
Wesley Chang <wesleycanada@gmail.com>
2019-01-27 12:24:54 -0800

    Added fare

--- nw_hacks_react/src/modules/Components/Side/Side.js
+++ nw_hacks_react/src/modules/Components/Side/Side.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import axios from 'axios';
+import wego from '../../../../src/WeGo.png';
+import savings from '../../../../src/savings.png';
+import co2e from '../../../../src/co2.png';
 import './Side.css'
 
 class Side extends Component {
@@ -10,7 +13,7 @@ class Side extends Component {
             signin: '',
             username: '',
             user_id: '',
-            show_login: true,
+            show_login: 0,
         }
     }
 
@@ -23,13 +26,13 @@ class Side extends Component {
             this.setState({
                 username: resp.username,
                 user_id: resp.userID,
-                show_login: false,
+                show_login: 1,
             })
         }).catch({});
     }
 
     login() {
-        if (!this.state.show_login) {
+        if (this.state.show_login !== 0) {
             return '';
         }
 
@@ -57,13 +60,13 @@ class Side extends Component {
     }
 
     control() {
-        if (this.state.show_login) {
+        if (this.state.show_login !== 1) {
             return '';
         }
 
         return (
             <div>
-                <h1 className="">From</h1><br />
+                <h2 className="">From</h2>
                 <input
                     type='text'
                     className='form-control'
@@ -77,8 +80,8 @@ class Side extends Component {
                     readOnly
                 />
                 <button className='btn btn-primary float-right' onClick={() => this.props.enableDraw(true)}>
-                Choose start location</button><br/><br/>
-                <h1 className="">To</h1><br />
+                Choose start location</button>
+                <h2 className="">To</h2><br />
                 <input
                     type='text'
                     className='form-control'
@@ -116,6 +119,9 @@ class Side extends Component {
                             "expires": 5,
                         }).then((e) => {
                             console.log(e)
+                            this.setState({
+                                show_login: 2,
+                            })
                         })
                     }
                     }
@@ -125,16 +131,82 @@ class Side extends Component {
         );
     }
 
+    processing() {
+        if (this.state.show_login !== 2) {
+            return '';
+        }
+
+        setTimeout(() => {
+            this.setState({
+                show_login: 3
+            })
+        }, 5000);
+
+        return (
+            <h1 className="">Processing...</h1>
+        )
+    }
+
+    payment() {
+        if (this.state.show_login !== 3) {
+            return '';
+        }
+
+        let fare;
+        let co2;
+
+        axios.post('http://localhost:8080/api/getSavings', {
+            "username": this.state.username,
+        }).then((e) => {
+            console.log(e)
+            fare = e.data.payload.costSavings;
+            co2 = e.data.payload.emissionSavings;
+        })
+
+        return (
+            <div>
+                <h1 className="">Found A Ride!</h1>
+                <img src = {savings} className='icon' style ={{
+                    width: '25%',
+                }} />Fare Saved:
+                <input
+                    type='text'
+                    className='form-control'
+                    value={'$3.35'}
+                    readOnly
+                />
+                <img src = {co2e} className='icon' style ={{
+                    width: '25%',
+                }} />
+                CO2 Emissions Saved:
+                <input
+                    type='text'
+                    className='form-control'
+                    value={'829.4g'}
+                    readOnly
+                />
+            </div>
+        )
+    }
 
 
     render() {
         return (
             <div className='col-lg-4 side'>
+            <img src = {wego} style ={{
+                    width: '75%',
+                }} />
                 {
                     this.login()
                 } 
                 {
                     this.control()
+                }
+                {
+                    this.processing()
+                }
+                {
+                    this.payment()
                 }
             </div>
         );

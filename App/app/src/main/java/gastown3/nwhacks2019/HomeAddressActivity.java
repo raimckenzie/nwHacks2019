8b571cb97b6aa7502ce1c78503ffa5d45ae0a600
jacqueschen1 <jacqueschen1@gmail.com>
2019-01-27 02:50:34 -0800

    added new activity

---
+++ App/app/src/main/java/gastown3/nwhacks2019/HomeAddressActivity.java
@@ -0,0 +1,13 @@
+package gastown3.nwhacks2019;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeAddressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_address);
+    }
+}
--- App/app/src/main/java/gastown3/nwhacks2019/LoginActivity.java
+++ App/app/src/main/java/gastown3/nwhacks2019/LoginActivity.java
@@ -213,6 +213,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(emailString, usernameString, passwordString);
             mAuthTask.execute((Void) null);
+            finish();
         }
 
 

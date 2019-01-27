7168f80ad4797173d35bf8f5e6fd1dc3c7656d65
jacqueschen1 <jacqueschen1@gmail.com>
2019-01-27 12:08:38 -0800

    fixed small error

--- App/app/src/main/java/gastown3/nwhacks2019/FurtherInfoActivity.java
+++ App/app/src/main/java/gastown3/nwhacks2019/FurtherInfoActivity.java
@@ -2,6 +2,9 @@ package gastown3.nwhacks2019;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import gastown3.nwhacks2019.server.Server;
 
 public class FurtherInfoActivity extends AppCompatActivity {
 
@@ -11,9 +14,14 @@ public class FurtherInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_further_info);
-
+        Server s = new Server("asdsda");
+        RideEvent[] res = s.getRequests();
         Position = getIntent().getIntExtra("POSITION", -1);
+        if(Position != -1) {
+            TextView text = findViewById(R.id.textView4);
 
-        
+        }
+
+
     }
 }
--- App/app/src/main/java/gastown3/nwhacks2019/MapsActivity2.java
+++ App/app/src/main/java/gastown3/nwhacks2019/MapsActivity2.java
@@ -121,7 +121,7 @@ public class MapsActivity2 extends AppCompatActivity
         Server s = new Server("http://5bcb1df1.ngrok.io/API/");
         s.getRequests(15,15);
 
-        Button takeHome = (Button) findViewById(R.id.button12);
+        Button takeHome = (Button) findViewById(R.id.button10);
         takeHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
